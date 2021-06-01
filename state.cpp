b5afa955324c5ba63fe59ff48ffcadaabff3b6b2
Agustin Morsant <79548109+mmorsant@users.noreply.github.com>
2021-06-01 13:59:28 -0300

    state.cpp or main.cpp
    Great and bigger updates comes in this commit!!:
    1. We can now spawn enemies randomly with only set the coordinate we want.
    1.1 Exists a number of limited enemies and it is very simply to create a new one.
    2. The enemies can move but only a few movements because the collision is not set and they can overflow the substract of the grid.
    3. Some tags and namespaces are new and modified searching for good documentation and a nice view of the code.
    4. I modify the "char" type of the ascii code tiles to "unsigned char" because it overflows the amount of size it can previously storage.

--- state.cpp
+++ state.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 namespace GRID
 {
@@ -20,70 +21,97 @@ namespace PLAYER
 
 namespace TILE
 {
-	constexpr char player = 64;
-	constexpr char floor = 176;
-	constexpr char wall = 219;
+	constexpr unsigned char player = 64;
+	constexpr unsigned char floor = 176;
+	constexpr unsigned char wall = 219;
+}
+
+namespace ENEMY
+{
+	struct enemy
+	{
+		int id;
+		unsigned char tile;
+		int posY;
+		int posX;
+	};
+
+	enemy swordsman{ 501, 159, 0, 0 };
+	enemy knight{ 502, 190, 0, 0 };
 }
 
 class GameSystem
 {
 private:
-	char** m_grid;
+	unsigned char** m_grid;
 	bool m_firstRun;
+	bool m_secondRun;
+	std::vector<ENEMY::enemy> m_enemies;
 
 public:
-	GameSystem() : m_firstRun{true}					//allocate memory and create actors
+	//Allocate memory actions for next functions, initialize all the resources.
+	GameSystem() : m_firstRun{ true }, m_secondRun{ true }
 	{
-		m_grid = new char*[GRID::height];			//rounds
-		for (int a{ 0 }; a < (GRID::height); a++)		//columns
+		m_grid = new unsigned char*[GRID::height];			//rounds
+		for (int a{ 0 }; a < (GRID::height); a++)			//columns
 		{
-			m_grid[a] = new char [GRID::width];
+			m_grid[a] = new unsigned char [GRID::width];
 		}
+
+		m_enemies.reserve(2);
 	}
 
-	void init()							//floor, wall and player tiles set
+	//Set tiles in the grid for floors, walls, players and enemies.
+	void init()
 	{
-		for (int a{ 0 }; a < (GRID::height); a++)		//rounds
+		//set floor all grid
+		for (int a{ 0 }; a < (GRID::height); a++)			//rounds
 		{
-			for (int b{ 0 }; b < (GRID::width); b++)	//columns
+			for (int b{ 0 }; b < (GRID::width); b++)		//columns
 			{
 				m_grid[a][b] = TILE::floor;
 			}
 		}
 
-		m_grid[PLAYER::positionY][PLAYER::positionX] = TILE::player;
-
 		//make default walls
-		for (int a{0}; a < (GRID::width); a++)			//superior
+		for (int a{ 0 }; a < (GRID::width); a++)				//superior
 		{
 			m_grid[0][a] = TILE::wall;
 		}
-		for (int a{0}; a < (GRID::height); a++)			//left
+		for (int a{ 0 }; a < (GRID::height); a++)				//left
 		{
 			m_grid[a][0] = TILE::wall;
 		}
-		for (int a{0}; a < (GRID::height); a++)			//right
+		for (int a{ 0 }; a < (GRID::height); a++)				//right
 		{
 			m_grid[a][GRID::width - 1] = TILE::wall;
 		}
-		for (int a{0}; a < (GRID::width); a++)			//inferior
+		for (int a{ 0 }; a < (GRID::width); a++)				//inferior
 		{
 			m_grid[GRID::height - 1][a] = TILE::wall;
 		}
+
+		//spawn enemies
+		spawnEnemy(6, 3);
+		spawnEnemy(6, 16);
+
+		//set player position
+		m_grid[PLAYER::positionY][PLAYER::positionX] = TILE::player;
 	}
 
-	bool state(int& input)						//player movement and draw
+	//Update a frame in the game status, manage the player and enemies movement while draw in the console.
+	bool state(unsigned short& input)
 	{
 		// > to second state put floor on the player was
 		if (m_firstRun == true)
 		{
 			m_firstRun = false;
 		}
-		else 
+		else
 		{
 			m_grid[PLAYER::positionY][PLAYER::positionX] = TILE::floor;
 		}
-		
+
 		//movement system
 		switch (input)
 		{
@@ -101,7 +129,7 @@ public:
 			break;
 		case 6:
 			//move right
-			if(PLAYER::positionX == GRID::extremeXplus)
+			if (PLAYER::positionX == GRID::extremeXplus)
 			{
 				m_grid[PLAYER::positionY][PLAYER::positionX] = TILE::player;
 			}
@@ -113,7 +141,7 @@ public:
 			break;
 		case 8:
 			//move up
-			if(PLAYER::positionY == GRID::extremeYminus)
+			if (PLAYER::positionY == GRID::extremeYminus)
 			{
 				m_grid[PLAYER::positionY][PLAYER::positionX] = TILE::player;
 			}
@@ -138,14 +166,15 @@ public:
 		case 5:
 			return false;
 		}
-		
-		//console output
+
+		moveEnemy();
 		draw();
 
 		return true;
 	}
 
-	void draw()							//console out the grid
+	//Output the grid values on Y and X.
+	void draw()
 	{
 		for (int a{ 0 }; a < (GRID::height); a++)
 		{
@@ -156,8 +185,85 @@ public:
 			std::cout << '\n';
 		}
 	}
+	
+	//Generate a random number between 501 and 502 (pre, not finished)
+	int random()
+	{
+		static short count{0};
 
-	~GameSystem()							//deallocate memory
+		if (count == 0)
+		{
+			count++;
+			return 501;
+		}
+		else if (count == 1)
+		{
+			count--;
+			return 502;
+		}
+		return 501;
+	}
+
+	//Set the spawn position Y and X, the function will spawn a random enemy on it.
+	void spawnEnemy(const int tempPosY, const int tempPosX, int tempId = 0, unsigned char tempTile = 0, unsigned int top = 0)
+	{
+		tempId = random();
+		top = (m_enemies.size());
+
+		if (tempId == ENEMY::swordsman.id)
+		{
+			tempTile = ENEMY::swordsman.tile;
+
+			m_enemies.push_back({ tempId, tempTile, tempPosY, tempPosX });
+			m_grid[tempPosY][tempPosX] = m_enemies[top].tile;
+		}
+		else if (tempId == ENEMY::knight.id)
+		{
+			tempTile = ENEMY::knight.tile;
+
+			m_enemies.push_back({ tempId, tempTile, tempPosY, tempPosX });
+			m_grid[tempPosY][tempPosX] = m_enemies[top].tile;
+		}
+	}
+
+	//Usig a predefined path, moves the enemy position every time that the players moves.
+	void moveEnemy()
+	{
+		//unable to move on the first run
+		if (m_secondRun == true)
+		{
+			m_secondRun = false;
+		}
+		else
+		{
+			for (unsigned short a{ 0 }; a < (m_enemies.size()); a++)
+			{
+				if ((m_enemies.at(a).id) == (ENEMY::swordsman.id))
+				{
+					//replace the tile where it last be with floor
+					m_grid[m_enemies[a].posY][m_enemies[a].posX] = TILE::floor;
+					//move the coords
+					m_enemies[a].posY++;
+					m_enemies[a].posX++;
+					//put the enemy tile in the new position
+					m_grid[m_enemies[a].posY][m_enemies[a].posX] = m_enemies[a].tile;
+				}
+				else if ((m_enemies.at(a).id) == (ENEMY::knight.id))
+				{
+					//replace the tile where it last be with floor
+					m_grid[m_enemies[a].posY][m_enemies[a].posX] = TILE::floor;
+					//move the coords
+					m_enemies[a].posY--;
+					m_enemies[a].posX--;
+					//put the enemy tile in the new position
+					m_grid[m_enemies[a].posY][m_enemies[a].posX] = m_enemies[a].tile;
+				}
+			}
+		}
+	}
+
+	//Deallocate all memory that is setup at the end of the game.
+	~GameSystem()
 	{
 		for (int a{ 0 }; a < (GRID::height); a++)
 		{
@@ -170,11 +276,10 @@ public:
 
 int main()
 {
+	unsigned short key{ 0 };
+
 	GameSystem run;
 	run.init();
-
-	int key{ 0 };
-
 	while (run.state(key))
 	{
 		key = 0;
