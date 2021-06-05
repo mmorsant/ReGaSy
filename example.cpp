cc1536b54a7b034fb0d29a3d0feb96c93cea8a21
Agustin Morsant <79548109+mmorsant@users.noreply.github.com>
2021-06-05 20:17:17 -0300

    New functions comes live!!!
    New features:
    1. setTiles function now accept a wall tile!!.
    2. Spawn player, player life and keys for movement setup is now real and its fancy and simple to work with.
    3. Update the "key" variable from ushort to uchar (allowing letter keys instead of only number keys).
    
    //PD: the uchar accepts ascii code whithout ' ' and the symbol or letters or numbers with ' '.

--- example.cpp
+++ example.cpp
@@ -2,19 +2,22 @@
 
 #include <mmorGS.h>
 
-int main ()
+int main()
 {
-  mmorGS game;
-  game.createWorld(10, 20);
-  game.setTiles(176);
-  
-  ushort key {0};
-  
-  while (game.frame(key, 5))
-  {
-    key = 0;
-    std::cin >> key;
-  }
-  
-  return 0;
+	mmorGS game;
+	game.createWorld(10, 20);
+	game.setTiles(' ', 219);
+	game.spawnPlayer( 64, 5, 5);
+	game.setPlayerLife(10);
+	game.setKeys( 'w', 's', 'a', 'd');
+
+	uchar key{0};
+
+	while (game.frame(key, '5'))
+	{
+		key = 0;
+		std::cin >> key;
+	}
+
+	return 0;
 }
