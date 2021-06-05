dd94d6d67b0dadce59f8230bbd1658ec3778598c
Agustin Morsant <79548109+mmorsant@users.noreply.github.com>
2021-06-05 20:09:40 -0300

    Implementation: new spawn, movement and collision.
    This days I'been working on a few details:
    1. New functions "spawnPlayer" "setPlayerLife" and "setKeys" that allows full setup on spawnpoint, life (for a future update) and movement keys for the player.
    2. New system of collision that prevent goingthrought the custom tile that the user setup it.
    3. Improvements on the memmory allocation type and grid error (it was char instead of unsigned char, allowing overflow the values of ascii).

--- lib/mmorGS.cpp
+++ lib/mmorGS.cpp
@@ -9,20 +9,21 @@ bool mmorGS::createWorld(ushort height, ushort width)
 	gs_height = height;
 	gs_width = width;
 
-	gs_grid = new char* [gs_height];
+	gs_grid = new uchar* [gs_height];
 	for (ushort a{ 0 }; a < (gs_height); a++)
 	{
-		gs_grid[a] = new char[gs_width];
+		gs_grid[a] = new uchar[gs_width];
 	}
 
 	return true;
 }
 
-bool mmorGS::setTiles(uchar floorTile)
+void mmorGS::setTiles(uchar floorTile, uchar wallTile)
 {
 	gs_floorTile = floorTile;
+	gs_wallTile = wallTile;
 
-	for (ushort a{ 0 }; a < (gs_height); a++)
+	for (ushort a{ 0 }; a < gs_height; a++)
 	{
 		for (ushort b{ 0 }; b < gs_width; b++)
 		{
@@ -30,20 +31,71 @@ bool mmorGS::setTiles(uchar floorTile)
 		}
 	}
 
+	//outter up wall
+	for (ushort a{ 0 }; a < gs_width; a++)
+	{
+		gs_grid[0][a] = gs_wallTile;
+	}
+	//outter down wall
+	for (ushort a{ 0 }; a < gs_width; a++)
+	{
+		gs_grid[(gs_height - 1)][a] = gs_wallTile;
+	}
+	//outter left wall
+	for (ushort a{ 0 }; a < gs_height; a++)
+	{
+		gs_grid[a][0] = gs_wallTile;
+	}
+	//outter right wall
+	for (ushort a{ 0 }; a < gs_height; a++)
+	{
+		gs_grid[a][(gs_width - 1)] = gs_wallTile;
+	}
+}
+
+bool mmorGS::spawnPlayer(uchar tile, ushort posY, ushort posX)
+{
+	gs_playerTile = tile;
+
+	gs_playerPosY = posY;
+	gs_playerPosX = posX;
+
+	gs_grid[gs_playerPosY][gs_playerPosX] = gs_playerTile;
+
 	return true;
 }
 
-bool mmorGS::frame(ushort input, ushort finish)
+void mmorGS::setPlayerLife(uint amountLife)
+{
+	gs_playerLife = amountLife;
+}
+
+void mmorGS::setKeys(uchar upKey, uchar downKey, uchar leftKey, uchar rightKey)
+{
+	gs_upKey = upKey;
+	gs_downKey = downKey;
+	gs_leftKey = leftKey;
+	gs_rightKey = rightKey;
+}
+
+bool mmorGS::frame(uchar input, uchar finish)
 {
 	if (gs_isFirstFrame)
 	{
 		gs_isFirstFrame = false;
 		draw();
+		return true;
 	}
 	else
 	{
 		if (input != finish)
 		{
+			if (!(collision(input, gs_wallTile)))
+			{
+				movePlayer(input);
+			}
+			else
+				NULL;
 			draw();
 			return true;
 		}
@@ -58,7 +110,7 @@ void mmorGS::draw()
 {
 	system("CLS");
 
-	for (ushort a{ 0 }; a < (gs_height); a++)
+	for (ushort a{ 0 }; a < gs_height; a++)
 	{
 		for (ushort b{ 0 }; b < gs_width; b++)
 		{
@@ -66,6 +118,63 @@ void mmorGS::draw()
 		}
 		std::cout << '\n';
 	}
+
+	std::cout << "Player Life: " << gs_playerLife << '\n';
+}
+
+bool mmorGS::movePlayer(uchar input)
+{
+	if (input == gs_upKey)
+	{
+		gs_grid[gs_playerPosY][gs_playerPosX] = gs_floorTile;
+		gs_playerPosY--;
+	}
+	else if (input == gs_downKey)
+	{
+		gs_grid[gs_playerPosY][gs_playerPosX] = gs_floorTile;
+		gs_playerPosY++;
+	}
+	else if (input == gs_leftKey)
+	{
+		gs_grid[gs_playerPosY][gs_playerPosX] = gs_floorTile;
+		gs_playerPosX--;
+	}
+	else if (input == gs_rightKey)
+	{
+		gs_grid[gs_playerPosY][gs_playerPosX] = gs_floorTile;
+		gs_playerPosX++;
+	}
+	else
+		return false;
+	gs_grid[gs_playerPosY][gs_playerPosX] = gs_playerTile;
+	return true;
+}
+
+bool mmorGS::collision(uchar input, uchar tileColl)
+{
+	ushort tempPosY = gs_playerPosY;
+	ushort tempPosX = gs_playerPosX;
+
+	if (input == gs_upKey)
+		tempPosY--;
+	else if (input == gs_downKey)
+		tempPosY++;
+	else if (input == gs_leftKey)
+		tempPosX--;
+	else if (input == gs_rightKey)
+		tempPosX++;
+	else
+		return false;
+
+	if ((gs_grid[tempPosY][tempPosX]) == tileColl)
+		return true;
+	else
+		return false;
+}
+
+bool mmorGS::damage()
+{
+	return false;
 }
 
 mmorGS::~mmorGS()
