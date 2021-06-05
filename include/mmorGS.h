28c6e985686740f2ad11a5ee7576f5195cbe691a
Agustin Morsant <79548109+mmorsant@users.noreply.github.com>
2021-06-05 20:09:41 -0300

    Declaration: new functions and improvements.
    In the header i look about those things:
    1. Instead of writing a duplicate of the member variables for a constructor, I prefer to create a default constructor and set the values directly in the declaration.
    2. Move the private part of the class to up the file because accesability and readability.

--- include/mmorGS.h
+++ include/mmorGS.h
@@ -3,34 +3,48 @@
 
 typedef unsigned short ushort;
 typedef unsigned char uchar;
+typedef unsigned int uint;
 
 class mmorGS
 {
+private:
+	ushort gs_height{10};
+	ushort gs_width{10};
+
+	uchar** gs_grid{nullptr};
+
+	ushort gs_playerPosY{1};
+	ushort gs_playerPosX{1};
+	uint gs_playerLife{5};
+
+	uchar gs_floorTile{176};
+	uchar gs_wallTile{219};
+	uchar gs_playerTile{64};
+
+	uchar gs_upKey{8};
+	uchar gs_downKey{2};
+	uchar gs_leftKey{4};
+	uchar gs_rightKey{6};
+
+	bool gs_isFirstFrame{true};
+
 public:
-	mmorGS() :	gs_height{ 10 }, 
-			gs_width{ 10 },
-			gs_grid{ nullptr },
-			gs_floorTile{ 176 },
-			gs_wallTile{ 219 },
-			gs_isFirstFrame{ true } {}
+	mmorGS() = default;
 
 	bool createWorld(ushort height = 10, ushort width = 10);
-	bool setTiles(uchar floorTile = 176);
-	bool frame(ushort input, ushort finish);
+	void setTiles(uchar floorTile = 176, uchar wallTile = 219);
+	bool spawnPlayer(uchar tile = 64, ushort posY = 1, ushort posX = 1);
+	void setPlayerLife(uint amountLife = 5);
+	void setKeys(uchar upKey, uchar downKey, uchar leftKey, uchar rightKey);
+
+	bool frame(uchar input, uchar finish);
 	void draw();
 
+	bool movePlayer(uchar input);
+	bool collision(uchar input, uchar tileColl);
+	bool damage();
+
 	~mmorGS();
-
-private:
-  ushort gs_height;
-  ushort gs_width;
-
-  char** gs_grid;
-
-  uchar gs_floorTile;
-  uchar gs_wallTile;
-
-  bool gs_isFirstFrame;
 };
 
 #endif
