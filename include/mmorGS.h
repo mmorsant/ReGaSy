db96a215916e2ada433490409af0f5f3754bc4a1
Agustin Morsant <79548109+mmorsant@users.noreply.github.com>
2021-06-06 03:35:22 -0300

    Declaration: new functions and improvements.
    Hey, how are you? I hope you're doing fine!, this is what i'been working on:
    1. Create all the enemy system (5 functions and 1 vector with a struct).
    2. Complete the damage and life to the player system.
    3. Reorganize some identifiers and settle down the spaces for "world setup", "player setup" and "enemy setup".

--- include/mmorGS.h
+++ include/mmorGS.h
@@ -1,6 +1,8 @@
 #ifndef mmorGS_h
 #define mmorGS_h
 
+#include <vector>
+
 typedef unsigned short ushort;
 typedef unsigned char uchar;
 typedef unsigned int uint;
@@ -10,21 +12,31 @@ class mmorGS
 private:
 	ushort gs_height{10};
 	ushort gs_width{10};
-
 	uchar** gs_grid{nullptr};
-
-	ushort gs_playerPosY{1};
-	ushort gs_playerPosX{1};
-	uint gs_playerLife{5};
 
 	uchar gs_floorTile{176};
 	uchar gs_wallTile{219};
 	uchar gs_playerTile{64};
 
+	ushort gs_playerPosY{ 1 };
+	ushort gs_playerPosX{ 1 };
+	int gs_playerLife{ 5 };
 	uchar gs_upKey{8};
 	uchar gs_downKey{2};
 	uchar gs_leftKey{4};
 	uchar gs_rightKey{6};
+
+	struct gs_enemy
+	{
+		ushort id;
+		uchar tile;
+		uint damage;
+		ushort posY;
+		ushort posX;
+		short moveY;
+		short moveX;
+	};
+	std::vector<gs_enemy> gs_enemies{};
 
 	bool gs_isFirstFrame{true};
 
@@ -33,16 +45,22 @@ public:
 
 	bool createWorld(ushort height = 10, ushort width = 10);
 	void setTiles(uchar floorTile = 176, uchar wallTile = 219);
-	bool spawnPlayer(uchar tile = 64, ushort posY = 1, ushort posX = 1);
-	void setPlayerLife(uint amountLife = 5);
-	void setKeys(uchar upKey, uchar downKey, uchar leftKey, uchar rightKey);
 
 	bool frame(uchar input, uchar finish);
 	void draw();
 
+	void spawnPlayer(uchar tile = 64, ushort posY = 1, ushort posX = 1);
+	void setPlayerLife(int amountLife = 5);
+	void setKeys(uchar upKey, uchar downKey, uchar leftKey, uchar rightKey);
 	bool movePlayer(uchar input);
-	bool collision(uchar input, uchar tileColl);
-	bool damage();
+	bool playerCollision(uchar input, uchar tileColl);
+	bool playerDamage(uchar input);
+
+	void createEnemyBuffer(uint size);
+	void createEnemy(uchar tile, uint damage, short moveY, short moveX);
+	void spawnEnemy(ushort id, ushort posY, ushort posX);
+	void moveEnemies();
+	bool enemyCollision(ushort id, int posY, int posX, uchar tileColl);
 
 	~mmorGS();
 };
